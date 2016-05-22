e1a2bc3e24cd91d073beaec76ea6e0f0848a30b2
Jack <lityangweiguang@163.com>
2016-05-22 23:02:40 +0800

    init Signed-off-by: Jack <lityangweiguang@163.com>

---
+++ vectorAdd.cu
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+__global__ void vectorAddKer(int *d_A, int *d_B, int *d_C, int size)
+{
+    int index = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if(index >= size) {
+        return;
+    }
+    d_C[index] = d_A[index] + d_B[index];
+}
+
+
+
+int main(int argc, char const *argv[]) {
+    int size = 0;
+    if(argc != 2) {
+        size = 1024;
+    } else {
+        size = atoi(argv[1]);
+    }
+
+    int *h_A, *h_B, *h_C;
+    int *d_A, *d_B, *d_C;
+
+    h_A = new int[size];
+    h_B = new int[size];
+    h_C = new int[size];
+
+    for(unsigned i = 0; i < size; ++i) {
+        h_A[i] = i;
+        h_B[i] = size - i;
+    }
+
+    cudaMalloc(&d_A, size * sizeof(int));
+    cudaMalloc(&d_B, size * sizeof(int));
+    cudaMalloc(&d_C, size * sizeof(int));
+
+    cudaMemcpy(d_A, h_A, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, h_B, size * sizeof(int), cudaMemcpyHostToDevice);
+
+    dim3 gridsize, blocksize;
+    blocksize.x = 256;
+    gridsize.x = (size + blocksize.x - 1) / blocksize.x;
+
+    vectorAddKer<<<gridsize, blocksize>>>(d_A, d_B, d_C, size);
+
+    cudaMemcpy(h_C, d_C, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+    for(unsigned i = 0; i < size; ++i) {
+        printf("%d + %d = %d\n", h_A[i], h_B[i], h_C[i]);
+    }
+
+    return 0;
+}
